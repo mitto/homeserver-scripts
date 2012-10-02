71daa91c5e5f07271221ed9a5173ee63798143d0
mitto nagisa <mitto@mittostar.info>
2012-10-02 13:43:28 +0900

    URLやパスワードを引数で指定できるように変更

--- logclear.rb
+++ logclear.rb
@@ -1,8 +1,12 @@
 # coding: utf-8
 require 'httpclient'
 
-url="http://192.168.10.100/sel.html?ClearEvent=Clear+Event+Log"
-
 client = HTTPClient.new
-client.set_auth(url, 'admin', 'admin')
-client.get_content(url)
+url = ARGV[0].to_s
+user = ARGV[1].to_s
+password = ARGV[2].to_s
+
+unless url.empty?
+  client.set_auth(url, user, password)
+  client.get_content(url)
+end
